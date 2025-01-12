36da9de1da19daeded1346ca6e52877b94495533
Yonatan Israeli <yonirisraeli@gmail.com>
2025-01-11 20:08:28 -0500

    Update Windows Bootloader Fixer.bat

--- Windows Bootloader Fixer.bat
+++ Windows Bootloader Fixer.bat
@@ -2,7 +2,7 @@
 setlocal
 title Windows Bootloader Fixer
 echo Program Name: Windows Bootloader Fixer
-echo Version: 4.0.34
+echo Version: 4.0.35
 echo License: GNU General Public License v3.0
 echo Developer: @YonatanReuvenIsraeli
 echo GitHub: https://github.com/YonatanReuvenIsraeli
