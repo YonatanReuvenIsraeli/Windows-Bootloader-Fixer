88579bfefc7a44e93df1c52f1c8c61cd9794303d
Yonatan Israeli <yonirisraeli@gmail.com>
2025-06-22 13:21:09 -0400

    Update Windows Bootloader Fixer.bat

--- Windows Bootloader Fixer.bat
+++ Windows Bootloader Fixer.bat
@@ -2,7 +2,7 @@
 title Windows Bootloader Fixer
 setlocal
 echo Program Name: Windows Bootloader Fixer
-echo Version: 5.0.3
+echo Version: 5.1.0
 echo License: GNU General Public License v3.0
 echo Developer: @YonatanReuvenIsraeli
 echo GitHub: https://github.com/YonatanReuvenIsraeli
@@ -503,10 +503,15 @@ goto "NewPartition"
 echo.
 set SurePartition=
 set /p SurePartition="All data on partition %Partition% will be deleted! Are you sure volume %Partition% is the boot partition? (Yes/No) "
-if /i "%SurePartition%"=="Yes" goto "Volume1"
+if /i "%SurePartition%"=="Yes" goto "BootloaderErrorWindowsErrorSet"
 if /i "%SurePartition%"=="No" goto "Partition"
 echo Invalid syntax!
 goto "SureBootPartition"
+
+:"BootloaderErrorWindowsErrorSet"
+set BootloaderError=
+set WindowsError=
+goto "Volume1"
 
 :"Volume1"
 if exist "diskpart.txt" goto "DiskPartExistVolume1"
@@ -553,7 +558,8 @@ echo.
 set BootAsk2=
 set /p BootAsk2="Is the boot volume %BootVolume% already assigned a drive letter? (Yes/No) "
 if /i "%BootAsk2%"=="Yes" goto "SureBootAsk2"
-if /i "%BootAsk2%"=="No" goto "WindowsDriveLetterSet"
+if /i "%BootloaderError%"=="True" if /i "%BootAsk2%"=="No" goto "BootloaderDriveLetter"
+if /i "%BootAsk2%"=="No" goto "Volume2"
 echo Invalid syntax!
 goto "BootAsk2"
 
@@ -566,9 +572,175 @@ if /i "%SureBootAsk2%"=="No" goto "BootAsk2"
 echo Invalid syntax!
 goto "SureBootAsk2"
 
+:"DriveLetterBootloader"
+echo.
+set DriveLetterBootloader=
+set /p DriveLetterBootloader="What is the drive letter of the boot volume? (A:-Z:) "
+if /i "%DriveLetterBootloader%"=="A:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="B:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="C:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="D:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="E:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="F:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="G:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="H:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="I:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="J:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="K:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="L:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="M:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="N:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="O:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="P:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="Q:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="R:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="S:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="T:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="U:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="V:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="W:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="X:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="Y:" goto "SureDriveLetterBootloader"
+if /i "%DriveLetterBootloader%"=="Z:" goto "SureDriveLetterBootloader"
+echo Invalid syntax!
+goto "DriveLetterBootloader"
+
+:"SureDriveLetterBootloader"
+echo.
+set SureDriveLetterBootloader=
+set /p SureDriveLetterBootloader="Are you sure "%DriveLetterBootloader%" is the boot volume drive letter? (Yes/No) "
+if /i "%SureDriveLetterBootloader%"=="Yes" goto "CheckExistDriveLetterBootloader"
+if /i "%SureDriveLetterBootloader%"=="No" goto "DriveLetterBootloader"
+echo Invalid syntax!
+goto "SureDriveLetterBootloader"
+
+:"CheckExistDriveLetterBootloader"
+if not exist "%DriveLetterBootloader%" goto "DriveLetterBootloaderNotExist"
+if /i "%BootloaderError%"=="True" goto "AssignDriveLetterBootloader"
+goto "Volume2"
+
+:"DriveLetterBootloaderNotExist"
+echo "%DriveLetterBootloader%" does not exist! Please try again.
+goto "Volume1"
+
+:"Volume2"
+if exist "diskpart.txt" goto "DiskPartExistVolume2"
+echo.
+echo Listing volumes in disk %Disk%.
+(echo list vol) > "diskpart.txt"
+(echo exit) >> "diskpart.txt"
+"%windir%\System32\diskpart.exe" /s "diskpart.txt" 2>&1
+if not "%errorlevel%"=="0" goto "Volume2Error"
+del "diskpart.txt" /f /q > nul 2>&1
+echo Volumes in disk %Disk% listed.
+goto "WindowsAsk1"
+
+:"DiskPartExistVolume2"
+set DiskPart=True
+echo.
+echo Please temporarily rename to something else or temporarily move to another location "diskpart.txt" in order for this batch file to proceed. "diskpart.txt" is not a system file. "diskpart.txt" is located in the folder "%cd%". Press any key to continue when "diskpart.txt" is renamed to something else or moved to another location. This batch file will let you know when you can rename it back to its original name or move it back to its original location.
+pause > nul 2>&1
+goto "Volume2"
+
+:"Volume2Error"
+del "diskpart.txt" /f /q > nul 2>&1
+echo There has been an error! Press any key to try again.
+pause > nul 2>&1
+goto "Volume2"
+
+:"WindowsAsk1"
+echo.
+set WindowsVolume=
+set /p WindowsVolume="What volume is the Windows volume? (0-?) "
+goto "SureWindowsAsk1"
+
+:"SureWindowsAsk1"
+echo.
+set SureWindowsAsk1=
+set /p SureWindowsAsk1="Are you sure volume %WindowsVolume% is the Windows volume? (Yes/No) "
+if /i "%SureWindowsAsk1%"=="Yes" goto "WindowsAsk2"
+if /i "%SureWindowsAsk1%"=="No" goto "Volume2"
+echo Invalid syntax!
+goto "SureWindowsAsk1"
+
+:"WindowsAsk2"
+echo.
+set WindowsAsk2=
+set /p WindowsAsk2="Is the Windows volume %WindowsVolume% already assigned a drive letter? (Yes/No) "
+if /i "%WindowsAsk2%"=="Yes" goto "SureWindowsAsk2"
+if /i "%WindowsError%"=="True" if /i "%WindowsAsk2%"=="No" goto "WindowsDriveLetter" 
+if /i "%BootAsk2%"=="Yes" if /i "%WindowsAsk2%"=="No" goto "WindowsDriveLetter"
+if /i "%BootAsk2%"=="No" if /i "%WindowsAsk2%"=="No" goto "BootloaderDriveLetter"
+echo Invalid syntax!
+goto "WindowsAsk2"
+
+:"SureWindowsAsk2"
+echo.
+set SureWindowsAsk2=
+set /p SureWindowsAsk2="Are you sure Windows volume %WindowsVolume% is already assigned a drive letter? (Yes/No) "
+if /i "%SureWindowsAsk2%"=="Yes" goto "DriveLetterWindows"
+if /i "%SureWindowsAsk2%"=="No" goto "WindowsAsk2"
+echo Invalid syntax!
+goto "SureWindowsAsk2"
+
+:"DriveLetterWindows"
+echo.
+set DriveLetterWindows=
+set /p DriveLetterWindows="What is the drive letter that Windows is installed on? (A:-Z:) "
+if /i "%DriveLetterWindows%"=="A:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="B:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="C:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="D:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="E:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="F:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="G:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="H:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="I:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="J:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="K:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="L:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="M:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="N:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="O:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="P:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="Q:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="R:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="S:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="T:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="U:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="V:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="W:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="X:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="Y:" goto "SureDriveLetterWindows"
+if /i "%DriveLetterWindows%"=="Z:" goto "SureDriveLetterWindows"
+echo Invalid syntax!
+goto "DriveLetterWindows"
+
+:"SureDriveLetterWindows"
+echo.
+set SureDriveLetterWindows=
+set /p SureDriveLetterWindows="Are you sure "%DriveLetterWindows%" is the drive letter that Windows is installed on? (Yes/No) "
+if /i "%SureDriveLetterWindows%"=="Yes" goto "CheckExistDriveLetterWindows"
+if /i "%SureDriveLetterWindows%"=="No" goto "DriveLetterWindows"
+echo Invalid syntax!
+goto "SureDriveLetterWindows"
+
+:"CheckExistDriveLetterWindows"
+if not exist "%DriveLetterWindows%" goto "DriveLetterWindowsNotExist"
+if not exist "%DriveLetterWindows%\Windows" goto "NotWindows"
+if /i "%WindowsError%"=="True" goto "AssignDriveLetterWindows" 
+if /i "%BootAsk2%"=="No" goto "BootloaderDriveLetter"
+goto "AssignDriveLetterBootloader"
+
+:"DriveLetterWindowsNotExist"
+echo "%DriveLetterWindows%" does not exist! Please try again.
+goto "Volume2"
+
+:"NotWindows"
+echo Windows not installed on "%DriveLetterWindows%"!
+goto "Volume2"
 
 :"WindowsDriveLetterSet"
-if /i "%BootloaderError%"=="True" goto "BootloaderDriveLetter"
 set WindowsDriveLetter=
 goto "BootloaderDriveLetter"
 
@@ -633,116 +805,9 @@ goto "BootloaderDriveLetter"
 
 :"AvailableDriveLetterFoundBootloader"
 echo Available drive letter found for the boot volume.
-goto "Volume2"
-
-:"DriveLetterBootloader"
-echo.
-set DriveLetterBootloader=
-set /p DriveLetterBootloader="What is the drive letter of the boot volume? (A:-Z:) "
-if /i "%DriveLetterBootloader%"=="A:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="B:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="C:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="D:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="E:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="F:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="G:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="H:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="I:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="J:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="K:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="L:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="M:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="N:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="O:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="P:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="Q:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="R:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="S:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="T:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="U:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="V:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="W:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="X:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="Y:" goto "SureDriveLetterBootloader"
-if /i "%DriveLetterBootloader%"=="Z:" goto "SureDriveLetterBootloader"
-echo Invalid syntax!
-goto "DriveLetterBootloader"
-
-:"SureDriveLetterBootloader"
-echo.
-set SureDriveLetterBootloader=
-set /p SureDriveLetterBootloader="Are you sure "%DriveLetterBootloader%" is the boot volume drive letter? (Yes/No) "
-if /i "%SureDriveLetterBootloader%"=="Yes" goto "CheckExistDriveLetterBootloader"
-if /i "%SureDriveLetterBootloader%"=="No" goto "DriveLetterBootloader"
-echo Invalid syntax!
-goto "SureDriveLetterBootloader"
-
-:"CheckExistDriveLetterBootloader"
-if not exist "%DriveLetterBootloader%" goto "DriveLetterBootloaderNotExist"
-goto "Volume2"
-
-:"DriveLetterBootloaderNotExist"
-echo "%DriveLetterBootloader%" does not exist! Please try again.
-goto "Volume1"
-
-:"Volume2"
 if /i "%BootloaderError%"=="True" goto "AssignDriveLetterBootloader"
-if exist "diskpart.txt" goto "DiskPartExistVolume2"
-echo.
-echo Listing volumes in disk %Disk%.
-(echo list vol) > "diskpart.txt"
-(echo exit) >> "diskpart.txt"
-"%windir%\System32\diskpart.exe" /s "diskpart.txt" 2>&1
-if not "%errorlevel%"=="0" goto "Volume2Error"
-del "diskpart.txt" /f /q > nul 2>&1
-echo Volumes in disk %Disk% listed.
-goto "WindowsAsk1"
-
-:"DiskPartExistVolume2"
-set DiskPart=True
-echo.
-echo Please temporarily rename to something else or temporarily move to another location "diskpart.txt" in order for this batch file to proceed. "diskpart.txt" is not a system file. "diskpart.txt" is located in the folder "%cd%". Press any key to continue when "diskpart.txt" is renamed to something else or moved to another location. This batch file will let you know when you can rename it back to its original name or move it back to its original location.
-pause > nul 2>&1
-goto "Volume2"
-
-:"Volume2Error"
-del "diskpart.txt" /f /q > nul 2>&1
-echo There has been an error! Press any key to try again.
-pause > nul 2>&1
-goto "Volume2"
-
-:"WindowsAsk1"
-echo.
-set WindowsVolume=
-set /p WindowsVolume="What volume is the Windows volume? (0-?) "
-goto "SureWindowsAsk1"
-
-:"SureWindowsAsk1"
-echo.
-set SureWindowsAsk1=
-set /p SureWindowsAsk1="Are you sure volume %WindowsVolume% is the Windows volume? (Yes/No) "
-if /i "%SureWindowsAsk1%"=="Yes" goto "WindowsAsk2"
-if /i "%SureWindowsAsk1%"=="No" goto "Volume2"
-echo Invalid syntax!
-goto "SureWindowsAsk1"
-
-:"WindowsAsk2"
-echo.
-set WindowsAsk=
-set /p WindowsAsk="Is the Windows volume %WindowsVolume% already assigned a drive letter? (Yes/No) "
-if /i "%WindowsAsk%"=="Yes" goto "SureWindowsAsk2"
-if /i "%WindowsAsk%"=="No" goto "WindowsDriveLetter"
-echo Invalid syntax!
-goto "WindowsAsk2"
-
-:"SureWindowsAsk2"
-echo.
-set SureWindowsAsk2=
-set /p SureWindowsAsk2="Are you sure Windows volume %WindowsVolume% is already assigned a drive letter? (Yes/No) "
-if /i "%SureWindowsAsk2%"=="Yes" goto "DriveLetterWindows"
-if /i "%SureWindowsAsk2%"=="No" goto "WindowsAsk2"
-echo Invalid syntax!
-goto "SureWindowsAsk2"
+if /i "%WindowsAsk2%"=="Yes" goto "AssignDriveLetterBootloader"
+if /i "%WindowsAsk2%"=="No" goto "WindowsDriveLetter"
 
 :"WindowsDriveLetter"
 echo.
@@ -805,62 +870,8 @@ goto "WindowsDriveLetter"
 
 :"AvailableDriveLetterFoundWindows"
 echo Available drive letter found for the Windows volume.
+if /i "%WindowsError%"=="True" goto "AssignDriveLetterWindows"
 goto "AssignDriveLetterBootloader"
-
-:"DriveLetterWindows"
-echo.
-set DriveLetterWindows=
-set /p DriveLetterWindows="What is the drive letter that Windows is installed on? (A:-Z:) "
-if /i "%DriveLetterWindows%"=="A:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="B:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="C:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="D:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="E:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="F:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="G:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="H:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="I:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="J:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="K:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="L:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="M:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="N:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="O:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="P:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="Q:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="R:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="S:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="T:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="U:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="V:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="W:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="X:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="Y:" goto "SureDriveLetterWindows"
-if /i "%DriveLetterWindows%"=="Z:" goto "SureDriveLetterWindows"
-echo Invalid syntax!
-goto "DriveLetterWindows"
-
-:"SureDriveLetterWindows"
-echo.
-set SureDriveLetterWindows=
-set /p SureDriveLetterWindows="Are you sure "%DriveLetterWindows%" is the drive letter that Windows is installed on? (Yes/No) "
-if /i "%SureDriveLetterWindows%"=="Yes" goto "CheckExistDriveLetterWindows"
-if /i "%SureDriveLetterWindows%"=="No" goto "DriveLetterWindows"
-echo Invalid syntax!
-goto "SureDriveLetterWindows"
-
-:"CheckExistDriveLetterWindows"
-if not exist "%DriveLetterWindows%" goto "DriveLetterWindowsNotExist"
-if not exist "%DriveLetterWindows%\Windows" goto "NotWindows"
-goto "AssignDriveLetterBootloader"
-
-:"DriveLetterWindowsNotExist"
-echo "%DriveLetterWindows%" does not exist! Please try again.
-goto "Volume2"
-
-:"NotWindows"
-echo Windows not installed on "%DriveLetterWindows%"!
-goto "Volume2"
 
 :"AssignDriveLetterBootloader"
 if /i "%WindowsError%"=="True" goto "AssignDriveLetterWindows"
@@ -879,8 +890,8 @@ del "diskpart.txt" /f /q > nul 2>&1
 if /i "%BootAsk2%"=="No" echo Drive letter "%BootloaderDriveLetter%" assigned to boot partition and boot partition "%BootloaderDriveLetter%" has been formatted.
 if /i "%BootAsk2%"=="Yes" echo Boot partition "%DriveLetterBootloader%" has been formatted.
 if /i "%BootAsk2%"=="No" set DriveLetterBootloader=%BootloaderDriveLetter%
-if /i "%WindowsAsk%"=="No" goto "AssignDriveLetterWindows"
-if /i "%WindowsAsk%"=="Yes" goto "Bootloader"
+if /i "%WindowsAsk2%"=="No" goto "AssignDriveLetterWindows"
+if /i "%WindowsAsk2%"=="Yes" goto "Bootloader"
 
 :"DiskPartExistAssignDriveLetterBootloader"
 set DiskPart=True
